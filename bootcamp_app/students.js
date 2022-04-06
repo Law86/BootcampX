2cb50f942031c246fcc649a4e042e7ccf3203014
Michael Law <mikelaw86@hotmail.com>
2022-04-06 17:12:24 -0400

    Add parameterization to student and teacher

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,13 +7,19 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const limit = process.argv[3] || 5;
+const cohortName = process.argv[2];
+const value = [`%${cohortName}%`, limit];
+const queryString = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
 WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+LIMIT $2;
+`;
+
+pool
+.query(queryString, value)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
